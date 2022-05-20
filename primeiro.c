bfddcc3b58a1a5017a57babe11bd4038e4cd23b0
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-05-20 15:22:33 -0300

    Create primeiro.c

---
+++ primeiro.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int main(){
+   int i, j;
+   float matriz[3][3]= {{1,2,3},
+                        {4,5,6},
+                        {7,8,9}};
+
+    float maior = matriz[0][0];
+
+    for (i=0; i<3; i++){
+        for (j=1; j<3; j++){
+            if(maior<matriz[i][j]){
+                maior = matriz [i][j];
+            }
+        }
+    }
+    printf("%f", maior);
+    return 0;
+}
